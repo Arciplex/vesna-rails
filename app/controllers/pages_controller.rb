114c0d96177aa37d8253775c1de9c395336af99d
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-10-10 21:20:18 -0500

    changes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def contact
+
+  end
+
+  def privacy
+
+  end
+
+  def product
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   resources :service_requests, only: [:new, :create]
 
+  match "/contact" => "pages#contact", via: :get
+  match "/privacy" => "pages#privacy", via: :get
+  match "/product" => "pages#product", via: :get
+
   root "welcome#index"
 
 end
